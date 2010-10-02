77a631d94d0cc51e097e90da68827322ec9f67c0
Steven Shaw <steshaw@gmail.com>
2010-10-02 20:45:18 +1000

    Grammar in progress

--- chap03/run.sh
+++ chap03/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-ml-build sources.cm Main.main &&
+# Explicit call to ml-yacc needed here as there is a bug in CM (Compilation Manager).
+ml-yacc tiger.grm &&
+  ml-build sources.cm Main.main &&
   sml @SMLload=sources.x86-linux > run.actual.out &&
   diff run.expected.out run.actual.out &&
   echo "good!"
