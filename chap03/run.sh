20dbfb5916ae67412787722b15fc7f331ebfa42e
Steven Shaw <steshaw@gmail.com>
2010-10-03 00:15:05 +1000

    Compile all test cases bar 3 with array index expressions. Array indexing conflicts with array creation

--- chap03/run.sh
+++ chap03/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+test20s=$(ls ../testcases/test2?.tig | grep -v ../testcases/test24.tig)
+test30s=$(ls ../testcases/test3?.tig | grep -v ../testcases/test30.tig)
+test40s=$(ls ../testcases/test4?.tig | grep -v ../testcases/test42.tig)
+
 # Explicit call to ml-yacc needed here as there is a bug in CM (Compilation Manager).
 ml-yacc tiger.grm &&
   ml-build sources.cm Main.main &&
@@ -8,6 +12,9 @@ ml-yacc tiger.grm &&
       *.tig \
       ../testcases/test?.tig \
       ../testcases/test1?.tig \
+      ${test20s} \
+      ${test30s} \
+      ${test40s} \
       > run.actual.out &&
   diff run.expected.out run.actual.out &&
   echo "good!"
