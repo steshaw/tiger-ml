139477a233ed82b562fe96b85f1ae1e58ac1cfc8
Steven Shaw <steshaw@gmail.com>
2010-10-03 12:09:30 +1000

    Simplify run.sh

--- chap03/run.sh
+++ chap03/run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-test20s=$(ls ../testcases/test2?.tig | grep -v ../testcases/test24.tig)
-test30s=$(ls ../testcases/test3?.tig | grep -v ../testcases/test30.tig)
-test40s=$(ls ../testcases/test4?.tig | grep -v ../testcases/test42.tig)
-
-test20s=$(ls ../testcases/test2?.tig)
-test30s=$(ls ../testcases/test3?.tig)
-test40s=$(ls ../testcases/test4?.tig)
-
 # Explicit call to ml-yacc needed here as there is a bug in CM (Compilation Manager).
 ml-yacc tiger.grm &&
   ml-build sources.cm Main.main &&
@@ -16,9 +8,9 @@ ml-yacc tiger.grm &&
       *.tig \
       ../testcases/test?.tig \
       ../testcases/test1?.tig \
-      ${test20s} \
-      ${test30s} \
-      ${test40s} \
+      ../testcases/test2?.tig \
+      ../testcases/test3?.tig \
+      ../testcases/test4?.tig \
       > run.actual.out &&
   diff run.expected.out run.actual.out &&
   echo "good!"
