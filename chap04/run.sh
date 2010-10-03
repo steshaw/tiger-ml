630ce2a38dd803c73f657900387e1c762b1f5052
Steven Shaw <steshaw@gmail.com>
2010-10-03 13:36:08 +1000

    Moved nil test to testcases. Introduce my tiger.grm with minimal changes

--- chap04/run.sh
+++ chap04/run.sh
@@ -5,7 +5,7 @@ ml-yacc tiger.grm &&
   ml-build sources.cm Main.main &&
   head -1 tiger.grm.desc &&
   sml @SMLload=sources.x86-linux \
-      nil.tig \
+      ../testcases/*.tig \
       > run.actual.out &&
   diff run.expected.out run.actual.out &&
   echo "good!"
