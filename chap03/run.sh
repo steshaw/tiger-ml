de3247ea0768cc9377d1a60e8591ee6a1aa540e3
Steven Shaw <steshaw@gmail.com>
2010-10-03 10:32:49 +1000

    Struggling with array_create/lvalue conflict

--- chap03/run.sh
+++ chap03/run.sh
@@ -4,6 +4,10 @@ test20s=$(ls ../testcases/test2?.tig | grep -v ../testcases/test24.tig)
 test30s=$(ls ../testcases/test3?.tig | grep -v ../testcases/test30.tig)
 test40s=$(ls ../testcases/test4?.tig | grep -v ../testcases/test42.tig)
 
+#test20s=$(ls ../testcases/test2?.tig)
+#test30s=$(ls ../testcases/test3?.tig)
+#test40s=$(ls ../testcases/test4?.tig)
+
 # Explicit call to ml-yacc needed here as there is a bug in CM (Compilation Manager).
 ml-yacc tiger.grm &&
   ml-build sources.cm Main.main &&
