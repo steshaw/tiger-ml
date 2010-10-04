9ad40b78567a4da1c712b7028e64d955ceab26a9
Steven Shaw <steshaw@gmail.com>
2010-10-04 23:35:45 +1000

    arithmetic and comparison expressions

--- chap04/run.sh
+++ chap04/run.sh
@@ -4,10 +4,16 @@
 ml-yacc tiger.grm &&
   ml-build sources.cm Main.main &&
   head -1 tiger.grm.desc &&
-  sml @SMLload=sources.x86-linux \
-      ../testcases/*.tig \
-      > run.actual.out &&
-  diff run.expected.out run.actual.out &&
-  echo "good!"
 
+(
+  for tigerSource in ../testcases/*.tig; do
+    echo -- $tigerSource --
+    cat $tigerSource
+    echo --
+    sml @SMLload=sources.x86-linux $tigerSource
+    echo
+  done
+) > run.actual.out
+
+diff run.expected.out run.actual.out && echo "good!"
 echo status=$?
