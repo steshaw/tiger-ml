c2bddce0d8fb5bada90c8e879653cf15be193c4d
Steven Shaw <steven@steshaw.org>
2011-04-07 09:34:47 +1000

    Changes for Mac/Darwin including hack to make ls sort order the same for testsuite runs

--- chap02/run.sh
+++ chap02/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+image=sources.x86-linux
+
+if [[ $(uname) = Darwin ]]; then
+  image=sources.x86-darwin
+fi
+
 ml-build sources.cm Main.main && 
-  sml @SMLload=sources.x86-linux > run.actual.out &&
+  sml @SMLload=${image} > run.actual.out &&
   diff run.expected.out run.actual.out &&
   echo "good!"
--- chap03/run.sh
+++ chap03/run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+image=sources.x86-linux
+
+if [[ $(uname) = Darwin ]]; then
+  image=sources.x86-darwin
+fi
+
 # Explicit call to ml-yacc needed here as there is a bug in CM (Compilation Manager).
 ml-yacc tiger.grm &&
   ml-build sources.cm Main.main &&
   head -1 tiger.grm.desc &&
-  sml @SMLload=sources.x86-linux \
+  sml @SMLload=${image} \
       ../testcases/*.tig \
       > run.actual.out &&
   diff run.expected.out run.actual.out &&
